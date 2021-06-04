b86d3857475ed4387df4c943289f2438606a2bc5
Christoph Sturm <me@christophsturm.com>
2021-06-04 12:03:13 +0200

    add some flow experiments.

--- failgood/src/test/kotlin/failgood/ContextProviderFlowTest.kt
+++ failgood/src/test/kotlin/failgood/ContextProviderFlowTest.kt
@@ -1,12 +1,33 @@
 package failgood
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.toCollection
 import org.junit.platform.commons.annotation.Testable
+import strikt.api.expectThat
+import strikt.assertions.containsExactly
+import java.util.concurrent.CopyOnWriteArrayList
 
 @Testable
 class ContextProviderFlowTest {
     val context = describe("Context providing implemented via flow") {
-        it("works") {
+        pending("works") {
+            val events = CopyOnWriteArrayList<String>()
+            coroutineScope {
+                val flow = flow {
+                    events.add("flow")
+                    emit(1)
+                    emit(2)
+                }
+                val sharedFlow = flow.shareIn(this, SharingStarted.Eagerly)
 
+                expectThat(sharedFlow.toCollection(mutableListOf())).containsExactly(1, 2)
+                expectThat(sharedFlow.toCollection(mutableListOf())).containsExactly(1, 2)
+                expectThat(events).containsExactly("flow")
+            }
         }
+
     }
 }
--- failgood/src/test/kotlin/failgood/TestFinderTest.kt
+++ failgood/src/test/kotlin/failgood/TestFinderTest.kt
@@ -4,7 +4,9 @@ import failgood.docs.ClassTestContextTest
 import failgood.docs.ObjectMultipleContextsTest
 import failgood.docs.ObjectTestContextTest
 import failgood.docs.TestContextExampleTest
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toCollection
+import kotlinx.coroutines.flow.toList
 import org.junit.platform.commons.annotation.Testable
 import strikt.api.expectThat
 import strikt.assertions.containsExactlyInAnyOrder
@@ -25,6 +27,29 @@ class TestFinderTest {
                         ObjectMultipleContextsTest::class,
                         TestContextExampleTest::class
                     )
+            }
+            it("runs async") {
+                val flow = FailGood.findTestClasses(classIncludeRegex = Regex(".*docs.*Test.class\$"))
+                val started = System.currentTimeMillis()
+                var firstItemTime: Long? = null
+                var lastItemTime: Long = 0
+                var firstItemTime2: Long? = null
+                var lastItemTime2: Long = 0
+                flow.map {
+                    if (firstItemTime == null)
+                        firstItemTime = System.currentTimeMillis()
+                    lastItemTime = System.currentTimeMillis()
+                    ObjectContextProvider(it).getContexts()
+                }.map {
+                    if (firstItemTime2 == null)
+                        firstItemTime2 = System.currentTimeMillis()
+                    lastItemTime2 = System.currentTimeMillis()
+                }.toList(mutableListOf())
+                println(
+                    "firstItem: ${firstItemTime!! - started} lastItem: ${lastItemTime - started} " +
+                            "firstItem2: ${firstItemTime2!! - started} lastItem2: ${lastItemTime2 - started} " +
+                            "end: ${System.currentTimeMillis() - started}"
+                )
             }
         }
 }
