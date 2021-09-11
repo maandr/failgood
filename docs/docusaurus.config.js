5c5e1e591e2ca127c1e40ba663b6565e58a6a3c1
Christoph Sturm <me@christophsturm.com>
2021-09-11 23:31:09 +0200

    fix docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -44,7 +44,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
                     items: [
                         {
                             type: 'doc',
-                            docId: 'intro',
+                            docId: 'intro/index',
                             position: 'left',
                             label: 'Tutorial',
                         },
@@ -64,7 +64,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
                             items: [
                                 {
                                     label: 'Tutorial',
-                                    to: '/docs/intro',
+                                    to: '/docs/intro/index',
                                 },
                             ],
                         },
